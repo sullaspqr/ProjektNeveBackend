d8ef433668233871635037153488c7f1c2a246ac
HaQT00R <40822691+sullaspqr@users.noreply.github.com>
2025-03-28 08:52:26 +0100

    Update BackendAlapContext.cs

--- Models/BackendAlapContext.cs
+++ Models/BackendAlapContext.cs
@@ -19,10 +19,10 @@ public partial class BackendAlapContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseMySQL("SERVER=localhost;PORT=3306;DATABASE=backend_alap;USER=root;PASSWORD=;SSL MODE=none;");
-
+*/
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Jogok>(entity =>
