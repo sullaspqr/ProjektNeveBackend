b7749e09daa541f8875d4516378c328f149c42bb
HaQT00R <40822691+sullaspqr@users.noreply.github.com>
2025-03-28 08:58:49 +0100

    Update LoggedUser.cs

--- DTOs/LoggedUser.cs
+++ DTOs/LoggedUser.cs
@@ -2,14 +2,14 @@
 {
     public class LoggedUser
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
         public int? Permission { get; set; }
 
-        public string ProfilePicturePath { get; set; }
+        public string? ProfilePicturePath { get; set; }
 
-        public string Token { get; set; }
+        public string? Token { get; set; }
     }
 }
