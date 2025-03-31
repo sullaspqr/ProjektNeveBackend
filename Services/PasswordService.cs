2aeaba57054981fc6bc899e5607d431480ae8405
HaQT00R <40822691+sullaspqr@users.noreply.github.com>
2025-03-31 13:42:57 +0200

    Create PasswordService.cs

---
+++ Services/PasswordService.cs
@@ -0,0 +1,9 @@
+public class PasswordService : IPasswordService
+{
+    public string HashPassword(string password)
+    {
+        // Implement your hashing logic (previously Program.CreateSHA256)
+    }
+
+    // Implement other methods
+}
